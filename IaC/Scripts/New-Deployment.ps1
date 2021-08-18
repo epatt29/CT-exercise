906de3502dff8af631b78f7899d88a3ce4bcdda9
epatt29 <epatt29@gmail.com>
2021-08-18 08:07:37 -0400

    Delete New-Deployment.ps1

--- IaC/Scripts/New-Deployment.ps1
+++
@@ -1,9 +0,0 @@
-$templateFile = "Templates\azuredeploy.json"
-$parameterFile="azuredeploy.parameters.json"
-$today=Get-Date -Format "MM-dd-yyyy"
-$deploymentName="VMDeployment-"+"$today"
-New-AzResourceGroupDeployment `
-  -ResourceGroupName "VMs_RSG"
-  -Name $deploymentName `
-  -TemplateFile $templateFile `
-  -TemplateParameterFile $parameterFile
